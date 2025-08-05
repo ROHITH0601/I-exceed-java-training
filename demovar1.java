5544cd5b0be702620578231b8df3d3551f71ed95
ROHITH0601 <137817919+ROHITH0601@users.noreply.github.com>
2025-08-06 00:24:31 +0530

    Create demovar1.java

---
+++ demovar1.java
@@ -0,0 +1,19 @@
+class sample {
+    public void iterate() {
+        for (var i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class demovar1 {
+    public static void main(String[] ads) {
+        new sample().iterate();
+        var x = 118;
+        var price = 32.43;
+        var name = "Rohith Kumar";
+        System.out.println(x);
+        System.out.println(price);
+        System.out.println(name);
+    }
+}
