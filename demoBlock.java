4f966608a4deeadaa081820e3529b3d5ff076deb
ROHITH0601 <137817919+ROHITH0601@users.noreply.github.com>
2025-08-06 09:30:53 +0530

    Update demoBlock.java

--- demoBlock.java
+++ demoBlock.java
@@ -2,10 +2,10 @@ class demoBlock
  {
  
     {
-        System.out.println("Instance block is called");
+        System.out.println("Instance the block is called and it is block level");
     }
     static{
-        System.out.println("inside static block");
+        System.out.println("inside the static block of the execution");
     }
  
     demoBlock() // constructor
@@ -15,6 +15,6 @@ class demoBlock
  
      public static void main(String[] args) {
        // new demoBlock();
-        System.out.println("Inside main mathod");
+        System.out.println("Inside the main method");
      }
  }
